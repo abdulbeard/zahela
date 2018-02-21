09c42af6e394be05132814fb0dce376099e75f23
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-02-21 01:56:18 -0500

    Fixing up the Friends component

--- frontend/src/app/components/friends/friends.component.ts
+++ frontend/src/app/components/friends/friends.component.ts
@@ -7,6 +7,7 @@ import { DisplayComment } from '../../models/DisplayComment';
 import * as $ from 'jquery';
 import { SlackReactionsService } from '../../services/SlackReactionsService';
 import { DisplayChannel } from '../../models/DisplayChannel';
+import { DisplayGuest, Gender } from '../../models/DisplayGuest';
 
 @Component({
   selector: 'app-home',
@@ -23,36 +24,33 @@ export class FriendsComponent implements AfterViewInit {
     private slackMessageParsingService: SlackMessageParsingService) {
   }
 
-  channels: DisplayChannel[] = [
-    new DisplayChannel("#random", "random", true),
-    new DisplayChannel("#general", "general", false)
-  ]
-
-  channelSelected(channel: DisplayChannel) {
-    var htmlElement = document.getElementById(channel.id);
-    this.changeActiveItemOnMenu(htmlElement);
-    this.showContent(channel);
-  }
-
-  private showContent(channel: DisplayChannel){
-    var allChannelContents = $('.channelContent').toArray();
-    allChannelContents.forEach(element => {
-      if(element.id === channel.channelContentId()){
-        $(element).show();
-      }
-      else{
-        $(element).hide();
-      }
-    });
-  }
-
-  private changeActiveItemOnMenu(item: HTMLElement) {
-    $(item)
-      .addClass('active')
-      .closest('.ui.menu')
-      .find('.item')
-      .not($(item))
-      .removeClass('active')
-      ;
-  }
+  friend: DisplayGuest = new DisplayGuest(
+    "Abdul Beard",
+    ["friends", "coworkers", "badass"],
+    "My super duper bestest friend. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    "https://www.facebook.com/abdulkhaliqzaheer",
+    "",
+    "",
+    "https://twosistersandamuslim.slack.com/messages/D2M36DM7T",
+    "yolo@solo.com",
+    ["coding", "philosophy", "IronMaiden"],
+    "../../../assets/profile_pic_1.jpg",
+    "../../../assets/profile_pic_2.jpg",
+    Gender.Female
+  );
+  friend1: DisplayGuest = new DisplayGuest(
+    "Abdul Beard",
+    ["friends", "coworkers", "badass"],
+    "My super duper bestest friend. Can't write much more bruh",
+    "https://www.facebook.com/abdulkhaliqzaheer",
+    "",
+    "",
+    "https://twosistersandamuslim.slack.com/messages/D2M36DM7T",
+    "yolo@solo.com",
+    ["coding", "philosophy", "IronMaiden"],
+    "../../../assets/profile_pic_1.jpg",
+    "../../../assets/profile_pic_2.jpg",
+    Gender.Female
+  );
+  friends: DisplayGuest[] = [this.friend, this.friend1, this.friend1, this.friend, this.friend1, this.friend, this.friend1];
 }
---
+++ frontend/src/app/models/DisplayGuest.ts
@@ -0,0 +1,72 @@
+export class DisplayGuest {
+    name: string;
+    categories: string[];
+    description: string;
+    facebookUrl: string;
+    twitterUrl: string;
+    instagramUrl: string;
+    slackUrl: string;
+    email: string;
+    interests: string[];
+    primaryImage: string;
+    secondaryImage: string;
+    gender: Gender;
+
+    constructor(name: string, categories: string[], description: string, facebookUrl: string, twitterUrl: string,
+        instagramUrl: string, slackUrl: string, email: string, interests: string[], primaryImage: string,
+        secondaryImage: string, gender: Gender) {
+        this.name = name
+        this.categories = categories
+        this.description = description
+        this.facebookUrl = facebookUrl
+        this.twitterUrl = twitterUrl
+        this.instagramUrl = instagramUrl
+        this.slackUrl = slackUrl
+        this.email = email
+        this.interests = interests
+        this.primaryImage = primaryImage
+        this.secondaryImage = secondaryImage
+        this.gender = gender;
+    }
+
+    public getMailToHref(): string {
+        return `mailto:${this.email}`;
+    }
+
+    public getInterestsText(): string {
+        var result = [];
+        for (var i = 0; i < this.interests.length; i++) {
+            if (i === 0) {
+                result.push(this.interests[i]);
+                continue;
+            }
+            else if (i > 0 && i < this.interests.length - 1) {
+                result.push(`, ${this.interests[i]}`);
+                continue;
+            }
+            else if (i === this.interests.length - 1) {
+                result.push(` and ${this.interests[i]}`);
+                continue;
+            }
+        }
+        return result.join("");
+    }
+
+    public getPronounText(firstCapital: boolean): string {
+        if (this.gender === Gender.Male) {
+            return firstCapital ? "He" : "he";
+        }
+        if (this.gender === Gender.Female) {
+            return firstCapital ? "She" : "she";
+        }
+        if (this.gender === Gender.NonSpecific) {
+            return firstCapital ? "They" : "they";
+        }
+    }
+}
+
+export enum Gender {
+    Male,
+    Female,
+    NonSpecific
+}
